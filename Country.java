97165278fc6248680592715048f5514acd0ab3ac
Mitesh908 <78071069+Mitesh908@users.noreply.github.com>
2023-08-03 10:06:39 +0530

    Create Country.java

---
+++ Country.java
@@ -0,0 +1,32 @@
+package com.mit.spcd.beans;
+
+import java.util.Map;
+
+public class Country {
+	
+	private String countryName;
+	private String currencyName;
+	private Map<String,String> states;
+	
+	public Country() {}
+	public String getCountryName() {
+		return countryName;
+	}
+	public void setCountryName(String countryName) {
+		this.countryName = countryName;
+	}
+	public String getCurrencyName() {
+		return currencyName;
+	}
+	public void setCurrencyName(String currencyName) {
+		this.currencyName = currencyName;
+	}
+	public Map<String,String> getStates() {
+		return states;
+	}
+	public void setStates(Map<String,String> states) {
+		this.states = states;
+	}
+	
+
+}
